ee0582883168a060379912bfc93b9a34a22f9592
mjeg1410 <michaeljohnedward@hotmail.com>
2020-03-10 16:32:57 +0000

    changed sensitive items to environment variables

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from os import getenv
 
 # create a new instance of Flask and store it in app 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.246.33.97/flask1'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = 'MY_SECRET_KEY'
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 # import the ./application/routes.py file
 from application import routes
