ce91f74b0c2a354d644a5ff4a6cfdb015dd7a6ca
mjeg1410 <michaeljohnedward@hotmail.com>
2020-03-12 15:13:55 +0000

    last changes

--- app.py
+++ app.py
@@ -4,16 +4,4 @@ from application import app
 if __name__ == '__main__':
  app.run(debug=True, host='0.0.0.0')
 
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', title ="Home Page")
-@app.route('/about')
-def about():
-    return render_template('about.html', title = "About Page")
 
